bdc58b3ea2fd4b0deb281ece3ea549c99fd342f9
Sayan6909 <148441906+Sayan6909@users.noreply.github.com>
2024-05-27 15:48:51 +0000

    Data  types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as Newer Version
+//alert(3+3) // we are using node js, not a browser
+
+console.log(3+3);
+
+
+let name="sayan";
+let age=33;
+let isLivenow=false;
+
+// Primitive datatypes:
+// number=>2^53
+// BigInt
+// string=>""
+// boolean=>trur or false
+// null=>stand alone value
+// undefined=>
+//symbol=>unique 
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
