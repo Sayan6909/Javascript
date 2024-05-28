3fc9bf43dec243bb433541d75a475bd9e7a1bdea
Sayan6909 <148441906+Sayan6909@users.noreply.github.com>
2024-05-28 13:00:40 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+// let score=true;
+// console.log(typeof (score));
+// console.log(typeof score);
+// node 01_basics/03_conversionoperation.js
+
+// let valueInnumber=Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33"=>33
+// "33abc"=>Nan
+// true=>1; false=>0
+
+// let isLoggedin="sayan";
+// let booleanIsloggedin=Boolean(isLoggedin)
+// console.log(booleanIsloggedin);
+
+// ""=>false
+// "sayan"=>true
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
