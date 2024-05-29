441a503cdb98a6731bce9a52c33f6baf8ec45861
Sayan6909 <148441906+Sayan6909@users.noreply.github.com>
2024-05-29 05:58:51 +0000

    conversion and operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,9 +1,9 @@
-// let score=true;
+let score=true;
 // console.log(typeof (score));
 // console.log(typeof score);
 // node 01_basics/03_conversionoperation.js
 
-// let valueInnumber=Number(score)
+let valueInnumber=Number(score)
 // console.log(typeof valueInnumber);
 // console.log(valueInnumber);
 
@@ -11,8 +11,8 @@
 // "33abc"=>Nan
 // true=>1; false=>0
 
-// let isLoggedin="sayan";
-// let booleanIsloggedin=Boolean(isLoggedin)
+let isLoggedin="sayan";
+let booleanIsloggedin=Boolean(isLoggedin)
 // console.log(booleanIsloggedin);
 
 // ""=>false
@@ -20,6 +20,39 @@
 
 let someNumber=33;
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************************Operations*********************************
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1=" Sayan "
+let str2=" Neha "
+
+let str3=str1+str2
+// console.log(str3);
+
+
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter);
+
+
 
