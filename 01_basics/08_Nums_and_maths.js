c0d2b508adf434bd2e47cf4cd596171567672168
Sayan6909 <148441906+Sayan6909@users.noreply.github.com>
2024-05-30 18:31:11 +0000

    Math and Num

---
+++ 01_basics/08_Nums_and_maths.js
@@ -0,0 +1,36 @@
+const score=400
+// console.log(score);
+
+const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber=23.662546
+
+// console.log(otherNumber.toPrecision(2))
+
+const hundreds=100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// *******************************************Maths*******************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.345)); //round off
+// console.log(Math.ceil(4.245));  //ceiling value
+// console.log(Math.floor(4.245));  //floor value
+// console.log(Math.min(4,6,2,5,4,6));  
+// console.log(Math.max(4,6,2,5,4,6));  
+
+// console.log(Math.random());   // always shows values from 0 to 1
+// console.log((Math.random()*10)+1);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
