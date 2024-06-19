780c3283e4e912002b4522b6caf85eff00784222
Sayan6909 <148441906+Sayan6909@users.noreply.github.com>
2024-06-19 07:51:10 +0000

    Truthy and falsy statements || Nullish coalesence and Ternary Operators

--- 04_ControlFlowInJS/03_truthy.js
+++ 04_ControlFlowInJS/03_truthy.js
@@ -21,4 +21,21 @@ if(Object.keys(empObj).length===0){
     console.log("object is empty")
 
 }
-// //
+// Nullish Coalecing operator(??)
+
+ let val1;
+// val1=5??10;
+// val1=null??10
+// val1=undefined??15
+
+val1=null??10??15
+console.log(val1);
+
+//Ternary Operator
+
+// condition?true:false
+
+const price=100;
+price<=80?console.log("Less than 80"):console.log("More than 80");
+
+
