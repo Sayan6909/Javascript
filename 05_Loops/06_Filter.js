27fcc173565d35db13c3f27152021e8cfa807f07
Sayan6909 <148441906+Sayan6909@users.noreply.github.com>
2024-06-20 04:02:26 +0000

    using filter

--- 05_Loops/06_Filter.js
+++ 05_Loops/06_Filter.js
@@ -9,12 +9,45 @@
 // })
 // console.log(values);
 
-const myNums=[1,2,3,4,5,6,7,8,9,10]
+// const myNums=[1,2,3,4,5,6,7,8,9,10]
 
 // const newNums= myNums.filter((num)=> num>4)
-const newNums= myNums.filter((nums)=>{
-    return nums>4
+// const newNums= myNums.filter((nums)=>{
+//     return nums>4
+// })
+// console.log(newNums)
+
+// const newNums=[]
+
+// myNums.forEach((num)=>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums)
+
+const books=[ 
+    { title:'Book One', genre:'Fiction', publish:1981,
+        edition:2004}, 
+    { title:'Book Two', genre:'Non-Fiction', publish:1981,
+        edition:2004},
+    { title:'Book Three', genre:'Science', publish:1983,
+        edition:2006},
+    { title:'Book Four', genre:'Personal Development', publish:2016,
+        edition:2017},
+    { title:'Book Five', genre:'Inspirational', publish:1984,
+        edition:2006},
+    { title:'Book Six', genre:'Spiritual', publish:1986,
+        edition:2007},
+    { title:'Book Seven', genre:'Non-Fiction', publish:2019,
+        edition:2020},
+    { title:'Book Eight', genre:'Science', publish:1987,
+        edition:2007},
+];
+
+let userBooks=books.filter((bk)=>bk.genre==='Science')
+userBooks=books.filter((bk)=>{
+    return bk.publish>=1982 && bk.genre==='Science'
 })
 
-
-console.log(newNums)
+console.log(userBooks);
